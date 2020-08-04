ba7d6c09ac1751d14334d13ff9cb45f255fb5e08
Javier Fernandez-Ivern <javier.ivern@gmail.com>
2020-08-04 16:32:41 -0700

    Fixed auth server port

--- src/auth.c
+++ src/auth.c
@@ -7,7 +7,8 @@
 #define MAX_POST_LENGTH 1024
 #define MAX_RESPONSE_LENGTH 1024
 
-size_t write_function(char *data, size_t size, size_t count, void *arg) {
+size_t write_function(char *data, size_t size, size_t count, void *arg)
+{
     size_t length = size * count;
     char *dst = (char *)arg;
     char *src = malloc(length + 1);
@@ -21,18 +22,19 @@ size_t write_function(char *data, size_t size, size_t count, void *arg) {
 int get_access_token(
     char *result, int length, char *username, char *identity_token)
 {
-    static char url[] = "http://craft.ivern.org:4000/api/identity";
+    static char url[] = "http://craft.ivern.org:8080/api/identity";
     strncpy(result, "", length);
     CURL *curl = curl_easy_init();
-    if (curl) {
+    if (curl)
+    {
         char post[MAX_POST_LENGTH] = {0};
         char response[MAX_RESPONSE_LENGTH] = {0};
         long http_code = 0;
         snprintf(post, MAX_POST_LENGTH, "username=%s&identity_token=%s",
-            username, identity_token);
-        #ifdef _WIN32
-            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-        #endif
+                 username, identity_token);
+#ifdef _WIN32
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+#endif
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_function);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
         curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -40,7 +42,8 @@ int get_access_token(
         CURLcode code = curl_easy_perform(curl);
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
         curl_easy_cleanup(curl);
-        if (code == CURLE_OK && http_code == 200) {
+        if (code == CURLE_OK && http_code == 200)
+        {
             strncpy(result, response, length);
             return 1;
         }
